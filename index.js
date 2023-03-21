fd36d3be30b377cb5ed6fb32400b837551ce1dc2
zyouxam <67564042+zyouxam@users.noreply.github.com>
2023-03-21 10:14:06 +0800

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 import * as THREE from 'https://unpkg.com/three@0.125.0/build/three.module.js';
 import { OrbitControls } from 'https://unpkg.com/three@0.125.0/examples/jsm/controls/OrbitControls.js';
 const scene = new THREE.Scene();
-const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 100000);
 
 // 创建渲染器
 const renderer = new THREE.WebGLRenderer();
